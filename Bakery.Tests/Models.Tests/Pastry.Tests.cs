f2bf5763c86cd5cc8bff4a031c689da6f5343097
osbrown1 <socknot@live.com>
2023-02-26 01:31:34 -0800

    working with right calculation from prompt example

--- Bakery.Tests/Models.Tests/Pastry.Tests.cs
+++ Bakery.Tests/Models.Tests/Pastry.Tests.cs
@@ -5,13 +5,13 @@ namespace Bakery.Tests
 {
   [TestClass]
   public class PastryTests
-  {
+{
 [TestMethod]
-    public void PastryConstructor_CalculatesPastryCost_12()
-    {
-      var pastry = new Pastry { Quantity = 8 };
+public void PastryConstructor_CalculatesPastryCost_12()
+{
+    var pastry = new Pastry { Quantity = 7 };
         decimal cost = pastry.CalculateCost();
         Assert.AreEqual(12m, cost);
-    }
-  }
+}
+}
 }
